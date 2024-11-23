4acf589ebc4fba0a4b76bf4ec1e029445550fd6d
Lasitha Nayajith <lnayanajith@hotmail.com>
2024-11-23 16:23:28 +0530

    add mock data for brackets

---
+++ src/mockData/brackets.ts
@@ -0,0 +1,135 @@
+export const tournarmentMockData = {
+    rounds:[
+        {
+            title:"Round of 16",
+            matches:[{
+                homeTeam:"Team 1",
+                homeTeamScore: 0,
+                homeTeamImageUrl:"https://placehold.co/80X80",
+                awayTeam:"Team 2",
+                awayTeamScore: 0,
+                awayTeamImageUrl:"https://placehold.co/80X80"
+            },{
+                homeTeam:"Team 1",
+                homeTeamScore: 0,
+                homeTeamImageUrl:"https://placehold.co/80X80",
+                awayTeam:"Team 2",
+                awayTeamScore: 0,
+                awayTeamImageUrl:"https://placehold.co/80X80"
+            },
+            {
+                homeTeam:"Team 1",
+                homeTeamScore: 0,
+                homeTeamImageUrl:"https://placehold.co/80X80",
+                awayTeam:"Team 2",
+                awayTeamScore: 0,
+                awayTeamImageUrl:"https://placehold.co/80X80"
+            },{
+                homeTeam:"Team 1",
+                homeTeamScore: 0,
+                homeTeamImageUrl:"https://placehold.co/80X80",
+                awayTeam:"Team 2",
+                awayTeamScore: 0,
+                awayTeamImageUrl:"https://placehold.co/80X80"
+            },
+            {
+                homeTeam:"Team 1",
+                homeTeamScore: 0,
+                homeTeamImageUrl:"https://placehold.co/80X80",
+                awayTeam:"Team 2",
+                awayTeamScore: 0,
+                awayTeamImageUrl:"https://placehold.co/80X80"
+            },{
+                homeTeam:"Team 1",
+                homeTeamScore: 0,
+                homeTeamImageUrl:"https://placehold.co/80X80",
+                awayTeam:"Team 2",
+                awayTeamScore: 0,
+                awayTeamImageUrl:"https://placehold.co/80X80"
+            },
+            {
+                homeTeam:"Team 1",
+                homeTeamScore: 0,
+                homeTeamImageUrl:"https://placehold.co/80X80",
+                awayTeam:"Team 2",
+                awayTeamScore: 0,
+                awayTeamImageUrl:"https://placehold.co/80X80"
+            },{
+                homeTeam:"Team 1",
+                homeTeamScore: 0,
+                homeTeamImageUrl:"https://placehold.co/80X80",
+                awayTeam:"Team 2",
+                awayTeamScore: 0,
+                awayTeamImageUrl:"https://placehold.co/80X80"
+            }
+        ]
+        },
+        {
+        title:"Quarter-finals",
+        matches:[{
+            homeTeam:"Team 1",
+            homeTeamScore: 0,
+            homeTeamImageUrl:"https://placehold.co/80X80",
+            awayTeam:"Team 2",
+            awayTeamScore: 0,
+            awayTeamImageUrl:"https://placehold.co/80X80"
+        },{
+            homeTeam:"Team 1",
+            homeTeamScore: 0,
+            homeTeamImageUrl:"https://placehold.co/80X80",
+            awayTeam:"Team 2",
+            awayTeamScore: 0,
+            awayTeamImageUrl:"https://placehold.co/80X80"
+        },
+        {
+            homeTeam:"Team 1",
+            homeTeamScore: 0,
+            homeTeamImageUrl:"https://placehold.co/80X80",
+            awayTeam:"Team 2",
+            awayTeamScore: 0,
+            awayTeamImageUrl:"https://placehold.co/80X80"
+        },{
+            homeTeam:"Team 1",
+            homeTeamScore: 0,
+            homeTeamImageUrl:"https://placehold.co/80X80",
+            awayTeam:"Team 2",
+            awayTeamScore: 0,
+            awayTeamImageUrl:"https://placehold.co/80X80"
+        }
+    ]
+    },{
+        title:"Semi-finals",
+        matches:[{
+            homeTeam:"Team 1",
+            homeTeamScore: 0,
+            homeTeamImageUrl:"https://placehold.co/80X80",
+            awayTeam:"Team 2",
+            awayTeamScore: 0,
+            awayTeamImageUrl:"https://placehold.co/80X80"
+        },
+        {
+            homeTeam:"Team 1",
+            homeTeamScore: 0,
+            homeTeamImageUrl:"https://placehold.co/80X80",
+            awayTeam:"Team 2",
+            awayTeamScore: 0,
+            awayTeamImageUrl:"https://placehold.co/80X80"
+        }
+    
+    ]
+
+    },
+    {
+        title:"Finals",
+        matches:[{
+            homeTeam:"Team 1",
+            homeTeamScore: 0,
+            homeTeamImageUrl:"https://placehold.co/80X80",
+            awayTeam:"Team 2",
+            awayTeamScore: 0,
+            awayTeamImageUrl:"https://placehold.co/80X80"
+        }]
+
+    }
+]
+}
